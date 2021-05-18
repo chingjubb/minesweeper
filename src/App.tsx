679e3fed6e728e69c92f8dc4a1717e8f6db3fe1f
bb <bb@bbs-MacBook.local>
2021-05-18 19:50:36 +0800

    Minesweeper init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import "./App.css";
-// import { YearLightPage } from './year_light_page';
-import { CeremonyLightPage } from "./ceremony_light_page";
+// import "./App.css";
+
+type Tile = {
+	isBomb: boolean;
+	row: number;
+	column: number;
+	clicked: boolean;
+	flagged: boolean;
+}
 
 function App() {
-  return <CeremonyLightPage />;
+	const numRow = 10;
+	const numColumn = 10;
+	const numBombs = 10;
+
+	// init the board
+	const board: Tile[][] = [];
+	for (let i = 0; i < numRow; i++) {
+		const thisRow: Tile[] = [];
+		for (let j = 0; j < numColumn; j++) {
+			thisRow[j] = { isBomb: false, row: i, column: j, clicked: false, flagged: false }; 
+		}
+		board.push(thisRow);
+	}
+
+	// Set bombs to random column and row
+	let numBombSet = 0;
+	while (numBombSet < numBombs) {
+		const randomRow = Math.floor(Math.random() * numRow);
+		const randomColumn = Math.floor(Math.random() * numColumn);
+		if (!board[randomRow][randomColumn].isBomb) {
+			board[randomRow][randomColumn].isBomb = true;
+			numBombSet++;
+		}
+	}
+	
+	console.log('board', board);
+	return <div>Mine sweeper</div>;
 }
+
+const Square = (props: Tile) => {
+	const { isBomb, row, column, clicked, flagged } = props;
+
+	return <div>{isBomb}</div>
+}
+
 
 export default App;
