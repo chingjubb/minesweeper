7a631563f561ac881ef732eedebdb56b0c39ff25
bb <usaki0502@gmail.com>
2021-05-20 16:51:47 +0800

    Add square snapshot tests

---
+++ src/square.test.tsx
@@ -0,0 +1,41 @@
+import renderer from 'react-test-renderer';
+import { Square } from './square';
+import { Tile,
+		 GameState,
+		} from './minesweeper_reducer';
+
+test('unclicked tile', () => {
+ 	const component = renderer.create(
+		<Square onRightClick={()=> {}} onClick={()=>{}} flagged={false} value={1} isBomb row={0} column={0} clicked={false} />,
+	);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('flagged tile', () => {
+ 	const component = renderer.create(
+		<Square onRightClick={()=> {}} onClick={()=>{}} flagged value={1} isBomb row={0} column={0} clicked={false} />,
+	);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('Bomb tile', () => {
+ 	const component = renderer.create(
+		<Square onRightClick={()=> {}} onClick={()=>{}} flagged={false} value={1} isBomb row={0} column={0} clicked />,
+	);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+describe('clicked tile ', ()=>{
+	for (let i = 0; i <= 8; i++) {
+		test(`clicked tile value = ${i}`, () => {
+		 	const component = renderer.create(
+				<Square onRightClick={()=> {}} onClick={()=>{}} flagged={false} value={i} isBomb={false} row={0} column={0} clicked />,
+			);
+			let tree = component.toJSON();
+			expect(tree).toMatchSnapshot();
+		});
+	}
+});
